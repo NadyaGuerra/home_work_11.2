15906004e811c3ea13636be4ed7391b2dc9b504c
Nadya Guerra <nadyammex@gmail.com>
2023-03-31 20:09:16 -0700

    2nd func added

--- str_func.py
+++ str_func.py
@@ -2,4 +2,6 @@ def str_func(message):
     '''Функция вывода заглавных букв'''
     return(message.upper())
 
-
+def title(message):
+    '''вывод первых заглавных букв'''
+    return(message.title())
